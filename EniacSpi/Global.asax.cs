3addb1998baaa4e211a6a69ec179d035e6e715ea
Lars Gardien <larsgardien@live.nl>
2016-05-22 13:55:39 +0200

    Added GoogleDriveService to Application dictionary

--- EniacSpi/Global.asax.cs
+++ EniacSpi/Global.asax.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.IO;
+using System.Threading;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -8,8 +14,54 @@ namespace EniacSpi
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private DriveService driveService;
+
+        public DriveService DriveService
+        {
+            get
+            {
+                return driveService ?? createDriveService();
+            }
+        }
+
+        private DriveService createDriveService()
+        {
+            UserCredential credential;
+
+            string[] Scopes = { DriveService.Scope.DriveReadonly };
+            string ApplicationName = "EniacSpi";
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/drive-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API service.
+            var service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            this.driveService = service;
+            return driveService;
+        }
+
         protected void Application_Start()
         {
+            Application["DriveService"] = DriveService;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
